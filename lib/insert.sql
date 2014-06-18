2a98674c39af9aadc22a18c6093641d5a84410b7
Katie Hoffman <ktahoffman@gmail.com>
2014-06-18 10:18:03 -0400

    adds encoded values

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,19 +1,19 @@
-INSERT INTO user (id, name, age) VALUES (1, "Finnebar", 17), (2, "Bear", 6), (3, "Iguana", 4), (4, "Alex", 33), 
-(5, "Amanda", 24), (6, "Sophie", 24), (7, "Rosey", 9), (8, "Victoria", 23), (9, "Franz", 100), (10, "Hermione", 30), 
-(11, "Voldemort", 90), (12, "Marisa", 24), (13, "Swizzle", 4), (14, "Sirius", 36), (15, "Albus", 113), (16, "Squid", 5), 
-(17, "Whale", 6), (18, "Pacha", 5), (19, "Ena", 24), (20, "Katie", 24);
+INSERT INTO user (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33), 
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30), 
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5), 
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO project (id, title, category, funding_goal, start_date, end_date) VALUES 
-(1, "Help me buy a guitar", "music", 500.00, "2013-06-30", "2013-07-30"), 
-(2, "My book on SQL", "books", 20.00, "2013-01-01", "2014-01-01"), 
-(3, "The next Harry Potter", "books", 1000.00, "2013-05-30", "2015-05-30"), 
-(4, "Animal shelter needs dog food", "charity", 400.00, "2013-03-14", "2013-06-30"), 
-(5, "Voldement needs a body", "charity", 6000.00, "2013-03-20", "2013-09-20"), 
-(6, "The next Inna-Gadda-Davida", "music", 200.00, "2014-12-30", "2015-12-30"), 
-(7, "Iguana needs tail operation", "charity", 2000.00, "2013-10-02", "2013-10-30"), 
-(8, "I have bed bugs!", "charity", 800.00, "2014-06-30", "2014-06-31"), 
-(9, "I want to teach English in China", "charity", 3000.00, "2013-06-30", "2013-09-30"), 
-(10, "Help save birds of paradise", "charity", 5000.00, "2012-03-20", "2013-06-30");
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'), 
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'), 
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'), 
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'), 
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'), 
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'), 
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'), 
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'), 
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'), 
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
 INSERT INTO pledge (id, amount, user_id, project_id) VALUES
 (1, 10.00, 1, 2), 
