c9f432d1dd3ec95066646e9155a1e45a4e9f8b46
Katie Hoffman <ktahoffman@gmail.com>
2014-06-20 09:09:46 -0400

    removes answers

--- spec/crowdfunder_spec.rb
+++ spec/crowdfunder_spec.rb
@@ -95,27 +95,27 @@ describe "crowdfunder sql" do
     end
 
     it 'selects the titles of all projects and their pledge amounts' do
-      expect(@db.execute("SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;")).to eq([["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise", 170], ["I have bed bugs!", 740], ["I want to teach English in China", 200], ["Iguana needs tail operation", 1035.5], ["My book on SQL", 20], ["The next Harry Potter", 120], ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]])
+      expect(@db.execute("your code here")).to eq([["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise", 170], ["I have bed bugs!", 740], ["I want to teach English in China", 200], ["Iguana needs tail operation", 1035.5], ["My book on SQL", 20], ["The next Harry Potter", 120], ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]])
     end
 
     it 'selects the user name, age, and pledge amount for all pledges' do
-      expect(@db.execute("SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;")).to eq([["Albus", "113", 470], ["Alex", "33", 20], ["Amanda", "24", 40], ["Bear", "6", 50], ["Ena", "24", 100], ["Finnebar", "17", 70], ["Franz", "100", 90], ["Hermione", "30", 50], ["Iguana", "4", 10], ["Katie", "24", 170], ["Marisa", "24", 24], ["Pacha", "5", 60], ["Rosey", "9", 50], ["Sirius", "36", 19], ["Sophie", "24", 60], ["Squid", "5", 270], ["Swizzle", "4", 12], ["Victoria", "23", 1700], ["Voldemort", "90", 34], ["Whale", "6", 125.5]])
+      expect(@db.execute("your code here")).to eq([["Albus", "113", 470], ["Alex", "33", 20], ["Amanda", "24", 40], ["Bear", "6", 50], ["Ena", "24", 100], ["Finnebar", "17", 70], ["Franz", "100", 90], ["Hermione", "30", 50], ["Iguana", "4", 10], ["Katie", "24", 170], ["Marisa", "24", 24], ["Pacha", "5", 60], ["Rosey", "9", 50], ["Sirius", "36", 19], ["Sophie", "24", 60], ["Squid", "5", 270], ["Swizzle", "4", 12], ["Victoria", "23", 1700], ["Voldemort", "90", 34], ["Whale", "6", 125.5]])
     end
 
     it 'selects the titles of all projects that have met their funding goal' do
-      expect(@db.execute("SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left <= 0;")).to eq([["My book on SQL", 0], ["The next Inna-Gadda-Davida", -142]])
+      expect(@db.execute("your code here")).to eq([["My book on SQL", 0], ["The next Inna-Gadda-Davida", -142]])
     end
 
     it 'selects user names and amounts of all pledges then orders them by the amount' do 
-      expect(@db.execute("SELECT users.name, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY SUM(pledges.amount);")).to eq([["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]])   
+      expect(@db.execute("your code here")).to eq([["Iguana", 10], ["Swizzle", 12], ["Sirius", 19], ["Alex", 20], ["Marisa", 24], ["Voldemort", 34], ["Amanda", 40], ["Bear", 50], ["Hermione", 50], ["Rosey", 50], ["Pacha", 60], ["Sophie", 60], ["Finnebar", 70], ["Franz", 90], ["Ena", 100], ["Whale", 125.5], ["Katie", 170], ["Squid", 270], ["Albus", 470], ["Victoria", 1700]])   
     end
 
     it 'selects the category names, and pledge amounts of all pledges in the music category' do
-      expect(@db.execute("SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music';")).to eq([["music", 40], ["music", 24], ["music", 34], ["music", 12], ["music", 40], ["music", 40], ["music", 20], ["music", 230]])
+      expect(@db.execute("your code here")).to eq([["music", 40], ["music", 24], ["music", 34], ["music", 12], ["music", 40], ["music", 40], ["music", 20], ["music", 230]])
     end
 
     it 'selects the category names and the sum total of the pledge amounts of all the pledges in the book category' do
-      expect(@db.execute("SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books' GROUP BY projects.category;")).to eq([["books", 140]])
+      expect(@db.execute("your code here")).to eq([["books", 140]])
     end
   end
 end
