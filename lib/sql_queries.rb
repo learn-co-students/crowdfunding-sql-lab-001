9ed181ec062a46b448cfb43c3375ce2740f8ee0f
BenjaGross <benja.j.gross@gmail.com>
2015-06-09 14:12:50 -0400

    adds example to sql_queries.rb

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,10 @@
-# Write your sql queries in this file in the appropriate method.
+# Write your sql queries in this file in the appropriate method like the example below:
 # 
+# def select_category_from_projects
+# "SELECT category FROM projects;"
+# end
+
+# Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
 "Write your SQL query Here"
